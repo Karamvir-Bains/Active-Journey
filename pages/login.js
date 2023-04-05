800020e0d33bb3993f84368b647fe272bfb9e9bb
Lauren Ashley <la.zaubi@gmail.com>
2023-04-04 17:15:11 -0700

    restructure login and reg forms to be inside of components, will use modes

--- pages/login.js
+++ pages/login.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
-import LoginForm from "./_login.form"
-import RegistrationForm from './_registration.form'
+import LoginForm from './components/LoginForm'
+import RegistrationForm from './components/RegistrationForm'
 
 export default function Login() {
   return (
@@ -9,13 +9,16 @@ export default function Login() {
         <title>User Login</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <main style={{
+      <main 
+        style={{
           backgroundImage: `url(/pexels-simon-berger-1323550.jpg)`,
           backgroundRepeat: 'no-repeat',
           backgroundSize: 'cover',
           backgroundPosition: 'bottom',
           // filter: 'brightness(90%)'
-        }} className="bg-slate-100 relative mx-auto w-full max-w-200 overflow-auto {styles.main}">
+        }}
+        className="bg-slate-100 relative mx-auto w-full max-w-200 overflow-auto {styles.main}"
+      >
         <LoginForm />
         {/* <RegistrationForm /> */}
       </main>
