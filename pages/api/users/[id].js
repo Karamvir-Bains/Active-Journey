f7832f34eafaf7882bef55d2f040251b66c1f2c9
Kristy Gislason <kristygislason@gmail.com>
2023-04-06 14:17:53 -0700

    Add security to not render the user json and only allow put

--- pages/api/users/[id].js
+++ pages/api/users/[id].js
@@ -2,15 +2,21 @@
 import prisma from '../../../lib/prisma';
 
 export default async function handler(req, res) {
-    const id = Number(req.query.id);
-    const updateLayout = await prisma.user.update({
-      where: {
-        id: id,
-      },
-      data: {
-        layout: JSON.stringify(req.body.data),
-      },
-    });
-    return await res.json(updateLayout);
+    if (req.method === 'PUT') {
+      const id = Number(req.query.id);
+      const updateLayout = await prisma.user.update({
+        where: {
+          id: id,
+        },
+        data: {
+          layout: JSON.stringify(req.body.data),
+        },
+      });
+      return await res.json(updateLayout);
+    } else {
+      return res.status(405).json({
+        'message': 'access is restricted'
+      })
+    }
 }
 
