f67e52f8747e988f91a5e5e35c1de7add07169dd
Karamvir-Bains <kbains2000@gmail.com>
2023-04-07 19:48:15 -0600

    Add ScaleComponent

---
+++ components/journal/ScaleComponent.js
@@ -0,0 +1,28 @@
+export default function ScaleComponent(props) {
+  return (
+    <div className="mb-6">
+      <div>
+        <label className="text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+          {props.name}
+        </label>
+      </div>
+
+      <div>
+        <input
+          className="w-full"
+          type="range"
+          min={0}
+          max={10}
+          step={1}
+          value={props.value || 5}
+          onChange={props.handleChange}
+        />
+        <div className="flex justify-between w-full">
+          <span>😔</span>
+          <span>😐</span>
+          <span>😊</span>
+        </div>
+      </div>
+    </div>
+  )
+}
