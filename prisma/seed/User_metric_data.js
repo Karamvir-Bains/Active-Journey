6c915eeda08aeaf804eea3f265683effcf2abfc4
Lauren Ashley <la.zaubi@gmail.com>
2023-04-10 17:30:05 -0700

    refactor prisma create fns

--- prisma/seed/User_metric_data.js
+++ prisma/seed/User_metric_data.js
@@ -2,6 +2,18 @@ const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient();
 
+async function createMetric(id, rangeVals, date) {
+  const value = Math.floor(Math.random() * (rangeVals[0] + rangeVals[1] - rangeVals[2])) + 1500;
+  await prisma.User_metric_data.create({
+    data: {
+      date,
+      metric_value: value,
+      user_id: 1,
+      metric_id: id
+    },
+  });
+}
+
 async function seed() {
 
   for (let i = 0; i < 365; i++) {
@@ -22,75 +34,27 @@ async function seed() {
       //print out fake data for each metric  
 
       if (metric.metric_id === 1) {
-        const water_value = Math.floor(Math.random() * (2500 - 1500 + 1)) + 1500;
-        await prisma.User_metric_data.create({
-          data: {
-            date,
-            metric_value: water_value,
-            user_id: 1,
-            metric_id: 1
-          },
-        });
+        await createMetric(1, [2500, 1500, 1], date);
       }
 
       if (metric.metric_id === 2) {
-        const sleep_value = Math.floor(Math.random() * (10 - 6 + 1)) + 6;
-        await prisma.User_metric_data.create({
-          data: {
-            date,
-            metric_value: sleep_value,
-            user_id: 1,
-            metric_id: 2
-          },
-        });
+        await createMetric(2, [10, 6, 1], date);
       }
 
       if (metric.metric_id === 3) {
-        const exercise_value = Math.floor(Math.random() * (60 - 15 + 1)) + 15;
-        await prisma.User_metric_data.create({
-          data: {
-            date,
-            metric_value: exercise_value,
-            user_id: 1,
-            metric_id: 3
-          },
-        });
+        await createMetric(3, [60, 15, 1], date);
       }
 
       if (metric.metric_id === 4) {
-        const energy_value = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
-        await prisma.User_metric_data.create({
-          data: {
-            date,
-            metric_value: energy_value,
-            user_id: 1,
-            metric_id: 4
-          },
-        });
+        await createMetric(4, [10, 1, 1], date);
       }
 
       if (metric.metric_id === 5) {
-        const mood_value = Math.floor(Math.random() * (3)) + 1;
-        await prisma.User_metric_data.create({
-          data: {
-            date,
-            metric_value: mood_value,
-            user_id: 1,
-            metric_id: 5
-          },
-        });
+        await createMetric(5, [3, 1, 1], date);
       }
 
       if (metric.metric_id === 6) {
-        const stress_value = Math.floor(Math.random() * (10)) + 1;
-        await prisma.User_metric_data.create({
-          data: {
-            date,
-            metric_value: stress_value,
-            user_id: 1,
-            metric_id: 6
-          },
-        });
+        await createMetric(6, [10, 1, 1], date);
       }
 
     }
