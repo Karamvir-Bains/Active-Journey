be2a1cf3ea875cc055f553a2ec5c53c70ee72fda
Kristy Gislason <kristygislason@gmail.com>
2023-04-10 22:54:27 -0700

    Adjust padding to match dashboard widgets

--- components/partials/_header.js
+++ components/partials/_header.js
@@ -9,7 +9,7 @@ export default function Header(props) {
         Hello {props.userName}
       </h2>
       <p>Welcome to ActiveJourney</p>
-      <nav className="flex content-center bg-blue-100 rounded-xl px-3 py-3 my-3">
+      <nav className="flex content-center bg-blue-100 rounded-xl px-5 py-3 my-3">
         <h1 className="self-center font-medium text-lg m-0">{props.pageTitle}</h1>
         <Link className="ml-auto self-end cursor-pointer items-center rounded-full bg-blue-800 hover:bg-blue-700 font-medium px-3 py-2.5 text-white" href="/profile">
           <div className="w-3 h-4">
