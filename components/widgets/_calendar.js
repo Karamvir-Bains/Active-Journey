cf93b2c194790ed4f62592d9849b4ce7ca11d203
Kristy Gislason <kristygislason@gmail.com>
2023-04-10 22:55:48 -0700

    Adjust formatting and fix error on value - covert from string to date

--- components/widgets/_calendar.js
+++ components/widgets/_calendar.js
@@ -12,24 +12,28 @@ export default function Calendar (props) {
   const dateFormatted = format(new Date(props.day), 'MMMM d,  yyyy')
   return (
     <div className='overflow-scroll rounded-lg bg-blue-200 shadow-sm w-full h-full p-6'>
-      <div className='flex flex-row justify-start content-center w-full'>
-        <button
-          onClick={() => props.setDay(today)}
-          className='flex flex-col justify-center content-center rounded-full text-blue-900 bg-blue-100 hover:bg-blue-50 py-1 px-5'
-        >
-          Today
-        </button>
-        <div className='text-blue-950 font-bold text-lg mx-auto'>
+      <div className='flex justify-evenly content-center w-full'>
+          <button
+            onClick={() => props.setDay(today)}
+            className='flex flex-col justify-center content-center rounded-full text-blue-900 bg-blue-100 hover:bg-blue-50 py-1 px-5 mr-auto'
+          >
+            Today
+          </button>
+        <div className='w-[50%] flex flex-col justify-self-center justify-center content-center text-blue-950 font-bold text-lg'>
           {formatDate(props.day)}
         </div>
-        <button className='flex flex-col ml-auto justify-center content-center rounded-full bg-blue-800 hover:bg-blue-700 py-1 px-3 text-white'>
+        <button className='flex flex-col justify-center content-center rounded-full bg-blue-800 hover:bg-blue-700 py-1 px-3 text-white ml-auto'>
           <div className='w-3 h-3'>
             <FontAwesomeIcon icon={faPlus} />
           </div>
-        </button>
+        </button>        
       </div>
       <div className='bg-white rounded-lg mt-3 p-2'>
-        <CalendarWidget value={props.day} onChange={e => props.setDay(e)} />
+        <CalendarWidget 
+          value={new Date(props.day)}
+          onChange={e => props.setDay(e)}
+          maxDate={today}
+        />
       </div>
     </div>
   )
