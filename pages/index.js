16e55a21050a609adab656a4a265867d6b5f8aaf
Lauren Ashley <la.zaubi@gmail.com>
2023-04-12 12:14:36 -0700

    update grid item config vals

--- pages/index.js
+++ pages/index.js
@@ -16,26 +16,26 @@ const defaultLayout = {
   lg: [
     { i: "overview", x: 0, y: 0, w: 8, h: 2, static: true},
     { i: "calendar", x: 9, y: 0, w: 4, h: 2, static: true},
-    { i: "dailyWater", x: 0, y: 0, w: 3, h: 2},
-    { i: "stress", x: 6, y: 7, w: 3, h: 2},
-    { i: "nutrition", x: 3, y: 8.5, w: 3, h: 2},
-    { i: "sleep", x: 6, y: 8.5, w: 6, h: 2},
-    { i: "activityGoal", x: 3, y: 7, w: 3, h: 2},
-    { i: "mood", x: 9, y: 7, w: 3, h: 2},
-    { i: "alcohol", x: 0, y: 8.5, w: 3, h: 2},
-    { i: "social", x: 3, y: 8.5, w: 6, h: 2}
+    { i: "dailyWater", x: 0, y: 3, w: 3, h: 2},
+    { i: "stress", x: 4, y: 3, w: 3, h: 2},
+    { i: "nutrition", x: 7, y: 3, w: 6, h: 2},
+    { i: "sleep", x: 0, y: 5, w: 6, h: 2},
+    { i: "activityGoal", x: 7, y: 5, w: 3, h: 2},
+    { i: "mood", x: 10, y: 7, w: 5, h: 2},
+    { i: "alcohol", x: 0, y: 7, w: 3, h: 2},
+    { i: "social", x: 4, y: 7, w: 3, h: 2}
   ],
   sm: [
     { i: "overview", x: 3, y: 0, w: 6, h: 2, static: true},
     { i: "calendar", x: 0, y: 0, w: 6, h: 2, static: true},
     { i: "dailyWater", x: 0, y: 0, w: 3, h: 2},
     { i: "stress", x: 0, y: 0, w: 3, h: 2},
-    { i: "nutrition", x: 3, y: 0, w: 6, h: 2},
+    { i: "nutrition", x: 4, y: 0, w: 6, h: 2},
     { i: "sleep", x: 0, y: 0, w: 6, h: 2},
-    { i: "activityGoal", x: 4, y: 0, w: 3, h: 2},
-    { i: "mood", x: 3, y: 0, w: 3, h: 2},
-    { i: "alcohol", x: 0, y: 0, w: 6, h: 2},
-    { i: "social", x: 3, y: 0, w: 6, h: 2}
+    { i: "activityGoal", x: 0, y: 0, w: 3, h: 2},
+    { i: "mood", x: 4, y: 0, w: 3, h: 2},
+    { i: "alcohol", x: 0, y: 0, w: 3, h: 2},
+    { i: "social", x: 4, y: 0, w: 3, h: 2}
   ]
 }
 
