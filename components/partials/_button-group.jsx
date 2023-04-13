78e283b91a18eeef3d52f359a68ae4e2b4869c0c
Lauren Ashley <la.zaubi@gmail.com>
2023-04-13 14:10:29 -0700

    clean up markup, handle styling classes

--- components/partials/_button-group.jsx
+++ components/partials/_button-group.jsx
@@ -14,18 +14,18 @@ export default function ButtonGroup(props) {
         // onClick={() => props.onClick(props.ranges[0])} 
         range={props.ranges[0]} 
         initial={true} 
+        classes="px-4 py-2 text-sm font-medium bg-white border border-gray-200 rounded-l-lg hover:bg-gray-100 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
       />
       <Button 
-        onClick={() => props.onClick(props.ranges[1])} 
-        range={props.ranges[1]} 
-        initial={false} 
+        onClick={() => setSelected(props.ranges[1])} 
+        range={props.ranges[1]}  
+        classes="px-4 py-2 text-sm font-medium text-gray-900 bg-white border-t border-b border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
       />
-      <span onClick={() => setSelected(props.ranges[1])} className="px-4 py-2 text-sm font-medium text-gray-900 bg-white border-t border-b border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
-        {props.ranges[1]}
-      </span>
-      <span onClick={() => setSelected(props.ranges[2])} className="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-r-md hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
-        {props.ranges[2]}
-      </span>
+      <Button
+        onClick={() => props.onClick(props.ranges[2])} 
+        range={props.ranges[2]}  
+        classes="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-r-md hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
+      />
     </div>
   )
 };
--- components/partials/_button.jsx
+++ components/partials/_button.jsx
@@ -1,7 +1,7 @@
 import { use, useState } from "react";
 
 export default function Button(props) {
-  const [isActive, setIsActive] = useState(props.initial);
+  const [isActive, setIsActive] = useState(props.initial || false);
 
   const handleClick = (event, range) => {
     setIsActive(true);
@@ -9,8 +9,11 @@ export default function Button(props) {
   }
 
   return (
-    <span onClick={() => handleClick(props.range)} className={`${isActive ? 'text-blue-700' : 'text-gray-900' } px-4 py-2 text-sm font-medium text-blue-700 bg-white border border-gray-200 rounded-l-lg hover:bg-gray-100 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white`}>
-      {props.range}
+    <span 
+      onClick={() => handleClick(props.range)} 
+      className={`${isActive ? 'text-blue-700' : 'text-gray-900' } ${props.classes}`}
+    >
+        {props.range}
     </span>
   )
 };
