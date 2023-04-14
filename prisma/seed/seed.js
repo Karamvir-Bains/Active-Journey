4121ea12984ca2e23458a9648e4081f86ad047e7
Kristy Gislason <kristygislason@gmail.com>
2023-04-13 22:19:29 -0700

    Add column to user table for storing dark_mode

--- prisma/seed/seed.js
+++ prisma/seed/seed.js
@@ -1,6 +1,4 @@
-import { defaultLayout } from '../helpers/data';
 const { PrismaClient } = require('@prisma/client');
-
 const prisma = new PrismaClient();
 
 async function reset() {
@@ -37,13 +35,13 @@ async function seed() {
       last_name: 'Doe',
       email: 'jane@jane.com',
       password: '12345',
-      layout: JSON.stringify(defaultLayout)
+      layout: '{"lg":[{"i":"overview","x":0,"y":0,"w":8,"h":2,"static":false},{"i":"calendar","x":9,"y":0,"w":4,"h":2,"static":true},{"i":"dailyWater","x":0,"y":0,"w":3,"h":2,"static":false},{"i":"activityGoal","x":3,"y":7,"w":3,"h":2,"static":false},{"i":"stress","x":6,"y":7,"w":3,"h":2,"static":false},{"i":"mood","x":9,"y":7,"w":3,"h":2,"static":false},{"i":"sleep","x":0,"y":9,"w":3,"h":2,"static":false},{"i":"social","x":3,"y":9,"w":3,"h":2,"static":false},{"i":"alcohol","x":6,"y":9,"w":6,"h":2,"static":false},{"i":"nutrition","x":0,"y":11,"w":6,"h":3,"static":false}],"sm":[{"i":"calendar","x":0,"y":0,"w":6,"h":2,"static":true},{"i":"overview","x":3,"y":0,"w":6,"h":2,"static":false},{"i":"dailyWater","x":0,"y":0,"w":3,"h":2,"static":false},{"i":"activityGoal","x":4,"y":0,"w":3,"h":2,"static":false},{"i":"stress","x":0,"y":0,"w":3,"h":2,"static":false},{"i":"mood","x":3,"y":0,"w":3,"h":2,"static":false},{"i":"sleep","x":0,"y":0,"w":6,"h":2,"static":false},{"i":"social","x":3,"y":0,"w":6,"h":2,"static":false},{"i":"alcohol","x":0,"y":0,"w":6,"h":2,"static":false},{"i":"nutrition","x":0,"y":0,"w":12,"h":3,"static":false}]}'
     },{
       first_name: 'Bluey',
       last_name: 'Heeler',
       email: 'bluey@heeler.aus',
       password: '54321',
-      layout: JSON.stringify(defaultLayout)
+      layout: '{"lg":[{"i":"overview","x":0,"y":0,"w":8,"h":2,"static":false},{"i":"calendar","x":9,"y":0,"w":4,"h":2,"static":true},{"i":"dailyWater","x":0,"y":0,"w":3,"h":2,"static":false},{"i":"activityGoal","x":3,"y":7,"w":3,"h":2,"static":false},{"i":"stress","x":6,"y":7,"w":3,"h":2,"static":false},{"i":"mood","x":9,"y":7,"w":3,"h":2,"static":false},{"i":"sleep","x":0,"y":9,"w":3,"h":2,"static":false},{"i":"social","x":3,"y":9,"w":3,"h":2,"static":false},{"i":"alcohol","x":6,"y":9,"w":6,"h":2,"static":false},{"i":"nutrition","x":0,"y":11,"w":6,"h":3,"static":false}],"sm":[{"i":"calendar","x":0,"y":0,"w":6,"h":2,"static":true},{"i":"overview","x":3,"y":0,"w":6,"h":2,"static":false},{"i":"dailyWater","x":0,"y":0,"w":3,"h":2,"static":false},{"i":"activityGoal","x":4,"y":0,"w":3,"h":2,"static":false},{"i":"stress","x":0,"y":0,"w":3,"h":2,"static":false},{"i":"mood","x":3,"y":0,"w":3,"h":2,"static":false},{"i":"sleep","x":0,"y":0,"w":6,"h":2,"static":false},{"i":"social","x":3,"y":0,"w":6,"h":2,"static":false},{"i":"alcohol","x":0,"y":0,"w":6,"h":2,"static":false},{"i":"nutrition","x":0,"y":0,"w":12,"h":3,"static":false}]}'
     }
   ];
 
