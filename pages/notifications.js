dddd3d3b29803eda5db142e960db947a6f5da57d
Karamvir-Bains <kbains2000@gmail.com>
2023-04-16 15:47:45 -0600

    Remove variables

--- pages/notifications.js
+++ pages/notifications.js
@@ -1,12 +1,8 @@
 import Layout from '../components/Layout'
 import Image from 'next/image'
 import { PrismaClient } from '@prisma/client'
-import { useApplicationData } from '../hooks/useApplicationData'
 
 export default function Settings (props) {
-  const { today, day, handleSetDay, data, setData, user, setUser, journalOpen, toggleJournal } =
-  useApplicationData();
-
   return (
     <Layout title="Notifications">
       <section className='mx-3 bg-white dark:bg-slate-900 dark:text-white  rounded-lg p-6 md:p-10'>
