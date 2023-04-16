398e61b7a29d79137ac7c1e1e439ea1e4c04a1cd
Karamvir-Bains <kbains2000@gmail.com>
2023-04-16 07:54:49 -0600

    Add userData api GET

---
+++ pages/api/userData.js
@@ -0,0 +1,33 @@
+import { PrismaClient } from '@prisma/client'
+
+export default async function handler(req, res) {
+  const prisma = new PrismaClient();
+  if (req.method === "GET") {
+    try {
+      const date = req.query.date;
+
+      // Start of day 90 days ago in UTC
+      const gte = new Date(new Date(date).toISOString().slice(0, 10) + "T00:00:00.000Z");
+      gte.setUTCDate(gte.getUTCDate() - 90);
+
+      // End of day in UTC
+      const lt = new Date(new Date(date).toISOString().slice(0, 10) + "T23:59:59.999Z");
+
+      const metrics = await prisma.metric.findMany({
+        include: {
+          user_metric_data: {
+            where: { date: { gte, lt } }
+          }
+        }
+      });
+
+      // Sending the metrics as JSON response
+      res.status(200).json({ metrics });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal server error' });
+    } finally {
+      await prisma.$disconnect();
+    }
+  }
+}
