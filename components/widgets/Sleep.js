390173e2ff6f2a86837cebf05367c30f6103c5c9
Kristy Gislason <kristygislason@gmail.com>
2023-04-15 23:30:50 -0700

    Noticed a few missing dark classes for new widgets

--- components/widgets/Sleep.js
+++ components/widgets/Sleep.js
@@ -50,8 +50,8 @@ export default function Sleep(props) {
   }, []);
   return(
     <>
-      <div className="rounded-lg bg-white shadow-sm w-full h-full p-6 mb-10 text-center">
-        <h3 className="font-bold mb-1 text-xl text-blue-900">Sleep vs Quality</h3>
+      <div className="rounded-lg bg-white dark:bg-slate-800 dark:text-white  shadow-sm w-full h-full p-6 mb-10 text-center">
+        <h3 className="font-bold mb-1 text-xl text-blue-900 dark:text-blue-500">Sleep vs Quality</h3>
         <div className="px-12">
           <canvas id='sleep'></canvas>
         </div>
