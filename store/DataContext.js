2a8c22cc89d82ba9ea63fec4f0aba36741267e85
Karamvir-Bains <kbains2000@gmail.com>
2023-04-16 07:49:01 -0600

    Add data and selectedDate

--- store/DataContext.js
+++ store/DataContext.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 
 const DataContext = React.createContext();
 
@@ -7,9 +7,28 @@ export function useData() {
 }
 
 export function DataProvider({ children }) {
+  const [data, setData] = useState([]);
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(`/api/userData?date=${selectedDate.toLocaleString()}`);
+      const { metrics } = await res.json();
+      setData(metrics);
+    }
+    fetchData();
+  }, [selectedDate]);
+
+  async function updateData(data) {
+    setData(data);
+  }
+
+  function updateDate(date) {
+    setSelectedDate(date);
+  }
 
   return (
-    <DataContext.Provider>
+    <DataContext.Provider value={{ data, updateData, selectedDate, updateDate }}>
       {children}
     </DataContext.Provider>
   );
