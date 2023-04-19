439a2cd9ad37228c31f59e53571bafc4332337ba
Lauren Ashley <la.zaubi@gmail.com>
2023-04-19 10:17:42 -0700

    hook up useContext data

--- components/widgets/Overview.js
+++ components/widgets/Overview.js
@@ -5,7 +5,6 @@ import { useData } from "../../store/DataContext";
 
 export default function Overview(props) {
   const { data } = useData();
-  const waterData = '';
 
   // Date range navigation
   const rangeValues = [7, 30, 90];
@@ -14,33 +13,39 @@ export default function Overview(props) {
     setRange(newRange);
   }
 
-  useEffect(() => {
-    const ctx = document.getElementById('myChart').getContext('2d');
+  // Array for each metric sliced to range of days
+  // takes metric data in order of chart datasets
+  const createData = useCallback((water, sleep, energy, mood) => {
+    const values = water.user_metric_data.slice(0, range);
+    const waterVals = values.map(e => Math.floor((e.metric_value / 100) - 10));
+      console.log('water: ', waterVals);
+    const sleepVals = sleep.user_metric_data
+      .slice(0, range)
+      .map(e => e.metric_value);
+    const energyVals = energy.user_metric_data
+      .slice(0, range)
+      .map(e => e.metric_value);
+    const moodVals = mood.user_metric_data
+      .slice(0, range)
+      .map(e => e.metric_value);
+    return [
+      waterVals,
+      sleepVals,
+      energyVals,
+      moodVals
+    ];
+  }, [range]); 
 
-    const entryIds = props.entries.map(entry => entry.id).slice(0, range);
-    const waterVals = props.water.map(entry => {
-      // Translate quantity vals to match 1-10 scale visually on y-axis
-      return (entry.metric_value / 100) - 15
-    }).slice(0, range);
-    const moodVals = props.mood.map(entry => entry.metric_value).slice(0, range);
-    const sleepVals = props.sleep.map(entry => entry.metric_value).slice(0, range);
-    const energyVals = props.energy.map(entry => entry.metric_value).slice(0, range);
-
-    // const {    
-    //   entryIds,
-    //   waterVals,
-    //   moodVals,
-    //   sleepVals,
-    //   energyVals } = createData();
-
-    var myChart = new Chart(ctx, {
+  const createChart = useCallback((metricValueSets) => {
+    const ctx = document.getElementById('overviewChart').getContext('2d');
+    return new Chart(ctx, {
       type: 'line',
       data: {
-        labels: entryIds,
+        labels: metricValueSets[0],
         datasets: [{
           type: 'line',
           label: "Mood",
-          data: moodVals,
+          data: metricValueSets[3],
           borderColor: "#c45850",
           pointRadius: 0,
           backgroundColor: "transparent",
@@ -49,20 +54,20 @@ export default function Overview(props) {
         }, {
           type: 'bar',
           label: "Water Intake",
-          data: waterVals,
+          data: metricValueSets[0],
           borderColor: "#3e95cd",
           borderRadius: 4,
           backgroundColor: "#3e95cd"
         }, {
           type: 'bar',
           label: "Sleep",
-          data: sleepVals,
+          data: metricValueSets[1],
           borderRadius: 4,
           backgroundColor: "#71d1bd"
         }, {
           type: 'line',
           label: "Energy Level",
-          data: energyVals,
+          data: metricValueSets[2],
           borderWidth: 0,
           pointRadius: 0,
           backgroundColor: "#ffc04d",
@@ -73,16 +78,41 @@ export default function Overview(props) {
       options: {
         responsive: true,
         maintainAspectRatio: false,
-        scales: { y: { display: false } },
+        scales: { 
+          y: { display: false },
+          // x: {
+          //   ticks: {
+          //     callback: function (chart) {
+          //       // chart.ticks = [];
+          //       // chart.ticks.push({ value: 0, label: "0 kWh" });
+          //       console.log('Ticks fn running', chart);
+          //       // return chart.ticks.filter((t, index) => {
+          //       //   // always show the first and the last tick
+          //       //   if (index === 0 || index === chart.ticks.length - 1) {
+          //       //     return t;
+          //       //   }
+          //       // });
+          //     }
+          //   }
+          // }
+        },
         plugins: { legend: { align: 'end' } }
       }
     });
+  }, []);
 
-    return () => {
-      myChart.destroy()
+  useEffect(() => {
+    if (data && data[0] && data[0].user_metric_data) {
+      const metricValueSets = createData(data[0], data[1], data[3], data[4]);
+
+      const overviewChart = createChart(metricValueSets);
+
+      return () => {
+        overviewChart.destroy()
+      }
     }
-  // }, [range, createData]);
-}, [range, props.entries, props.water, props.mood, props.sleep, props.energy]);
+    
+}, [range, data, createData, createChart]);
 
   return (
     <>
@@ -96,7 +126,7 @@ export default function Overview(props) {
           <span className="text-xs">&nbsp;days</span>
         </div>
         <div className="absolute top-4 right-3 bottom-2 left-3 z-0">
-          <canvas id='myChart'></canvas>
+          <canvas id='overviewChart'></canvas>
         </div>
       </div>
     </>
