7a9011b3a1bbfb5870975cf9cadccddc9e97363a
Karamvir-Bains <kbains2000@gmail.com>
2023-04-23 08:35:06 -0600

    Add min and max for layout

--- prisma/seed/User.js
+++ prisma/seed/User.js
@@ -4,52 +4,52 @@ const prisma = new PrismaClient()
 
 const layoutConfig = {
   xl: [
-    { w: 8, h: 2, x: 0, y: 0, i: 'overview', moved: false, static: false },
-    { w: 4, h: 2, x: 8, y: 0, i: 'calendar', moved: false, static: true },
-    { w: 3, h: 2, x: 0, y: 2, i: 'dailyWater', moved: false, static: false },
-    { w: 3, h: 2, x: 3, y: 2, i: 'activityGoal', moved: false, static: false },
-    { w: 3, h: 2, x: 6, y: 2, i: 'stress', moved: false, static: false },
-    { w: 3, h: 2, x: 9, y: 2, i: 'mood', moved: false, static: false },
-    { w: 6, h: 2, x: 0, y: 4, i: 'sleep', moved: false, static: false },
-    { w: 6, h: 2, x: 6, y: 4, i: 'social', moved: false, static: false },
-    { w: 6, h: 2, x: 6, y: 6, i: 'alcohol', moved: false, static: false },
-    { w: 6, h: 2, x: 0, y: 6, i: 'nutrition', moved: false, static: false }
+    { w: 9, h: 2, x: 0, y: 0, minW: 4, minH: 2, i: 'overview', moved: false, static: false },
+    { w: 3, h: 2, x: 9, y: 0, minW: 3, minH: 2, maxH: 2, i: 'calendar', moved: false, static: false },
+    { w: 3, h: 2, x: 0, y: 2, minW: 2, minH: 2, i: 'dailyWater', moved: false, static: false },
+    { w: 3, h: 2, x: 3, y: 2, minW: 2, minH: 2, i: 'activityGoal', moved: false, static: false },
+    { w: 3, h: 2, x: 6, y: 2, minW: 2, minH: 2, i: 'stress', moved: false, static: false },
+    { w: 3, h: 2, x: 9, y: 2, minW: 2, minH: 2, i: 'mood', moved: false, static: false },
+    { w: 6, h: 2, x: 0, y: 4, minW: 4, minH: 2, i: 'sleep', moved: false, static: false },
+    { w: 6, h: 2, x: 6, y: 4, minW: 4, minH: 2, i: 'social', moved: false, static: false },
+    { w: 6, h: 2, x: 6, y: 6, minW: 4, minH: 2, i: 'alcohol', moved: false, static: false },
+    { w: 6, h: 2, x: 0, y: 6, minW: 4, minH: 2, i: 'nutrition', moved: false, static: false }
   ],
   lg: [
-    { w: 7, h: 2, x: 0, y: 0, i: 'overview', moved: false, static: false },
-    { w: 5, h: 2, x: 7, y: 0, i: 'calendar', moved: false, static: true },
-    { w: 6, h: 2, x: 0, y: 2, i: 'dailyWater', moved: false, static: false },
-    { w: 6, h: 2, x: 6, y: 2, i: 'activityGoal', moved: false, static: false },
-    { w: 6, h: 2, x: 0, y: 4, i: 'stress', moved: false, static: false },
-    { w: 6, h: 2, x: 6, y: 4, i: 'mood', moved: false, static: false },
-    { w: 6, h: 2, x: 0, y: 6, i: 'sleep', moved: false, static: false },
-    { w: 6, h: 2, x: 6, y: 6, i: 'social', moved: false, static: false },
-    { w: 6, h: 2, x: 6, y: 8, i: 'alcohol', moved: false, static: false },
-    { w: 6, h: 2, x: 0, y: 8, i: 'nutrition', moved: false, static: false }
+    { w: 7, h: 2, x: 0, y: 0, minW: 6, minH: 2, i: 'overview', moved: false, static: false },
+    { w: 5, h: 2, x: 7, y: 0, minW: 5, minH: 2, maxH: 2, i: 'calendar', moved: false, static: false },
+    { w: 6, h: 2, x: 0, y: 2, minW: 3, minH: 2, i: 'dailyWater', moved: false, static: false },
+    { w: 6, h: 2, x: 6, y: 2, minW: 3, minH: 2, i: 'activityGoal', moved: false, static: false },
+    { w: 6, h: 2, x: 0, y: 4, minW: 3, minH: 2, i: 'stress', moved: false, static: false },
+    { w: 6, h: 2, x: 6, y: 4, minW: 3, minH: 2, i: 'mood', moved: false, static: false },
+    { w: 6, h: 2, x: 0, y: 6, minW: 6, minH: 2, i: 'sleep', moved: false, static: false },
+    { w: 6, h: 2, x: 6, y: 6, minW: 6, minH: 2, i: 'social', moved: false, static: false },
+    { w: 6, h: 2, x: 6, y: 8, minW: 6, minH: 2, i: 'alcohol', moved: false, static: false },
+    { w: 6, h: 2, x: 0, y: 8, minW: 6, minH: 2, i: 'nutrition', moved: false, static: false }
   ],
   md: [
-    { w: 6, h: 2, x: 0, y: 2, i: 'overview', moved: false, static: false },
-    { w: 6, h: 2, x: 0, y: 0, i: 'calendar', moved: false, static: true },
-    { w: 6, h: 2, x: 0, y: 4, i: 'dailyWater', moved: false, static: false },
-    { w: 6, h: 2, x: 4, y: 4, i: 'activityGoal', moved: false, static: false },
-    { w: 6, h: 2, x: 0, y: 6, i: 'stress', moved: false, static: false },
-    { w: 6, h: 2, x: 4, y: 6, i: 'mood', moved: false, static: false },
-    { w: 6, h: 2, x: 0, y: 8, i: 'sleep', moved: false, static: false },
-    { w: 6, h: 2, x: 0, y: 10, i: 'social', moved: false, static: false },
-    { w: 6, h: 2, x: 0, y: 12, i: 'alcohol', moved: false, static: false },
-    { w: 6, h: 2, x: 0, y: 14, i: 'nutrition', moved: false, static: false }
+    { w: 6, h: 2, x: 0, y: 2, minW: 4, minH: 2, i: 'overview', moved: false, static: false },
+    { w: 6, h: 2, x: 0, y: 0, minW: 3, minH: 2, maxH: 2, i: 'calendar', moved: false, static: false },
+    { w: 6, h: 2, x: 0, y: 4, minW: 2, minH: 2, i: 'dailyWater', moved: false, static: false },
+    { w: 6, h: 2, x: 4, y: 4, minW: 2, minH: 2, i: 'activityGoal', moved: false, static: false },
+    { w: 6, h: 2, x: 0, y: 6, minW: 2, minH: 2, i: 'stress', moved: false, static: false },
+    { w: 6, h: 2, x: 4, y: 6, minW: 2, minH: 2, i: 'mood', moved: false, static: false },
+    { w: 6, h: 2, x: 0, y: 8, minW: 4, minH: 2, i: 'sleep', moved: false, static: false },
+    { w: 6, h: 2, x: 0, y: 10, minW: 4, minH: 2, i: 'social', moved: false, static: false },
+    { w: 6, h: 2, x: 0, y: 12, minW: 4, minH: 2, i: 'alcohol', moved: false, static: false },
+    { w: 6, h: 2, x: 0, y: 14, minW: 4, minH: 2, i: 'nutrition', moved: false, static: false }
   ],
   sm: [
-    { w: 1, h: 2, x: 0, y: 2, i: 'overview', moved: false, static: false },
-    { w: 1, h: 2, x: 0, y: 0, i: 'calendar', moved: false, static: true },
-    { w: 1, h: 2, x: 0, y: 4, i: 'dailyWater', moved: false, static: false },
-    { w: 1, h: 2, x: 0, y: 6, i: 'activityGoal', moved: false, static: false },
-    { w: 1, h: 2, x: 0, y: 8, i: 'stress', moved: false, static: false },
-    { w: 1, h: 2, x: 0, y: 10, i: 'mood', moved: false, static: false },
-    { w: 1, h: 2, x: 0, y: 12, i: 'sleep', moved: false, static: false },
-    { w: 1, h: 2, x: 0, y: 14, i: 'social', moved: false, static: false },
-    { w: 1, h: 2, x: 0, y: 16, i: 'alcohol', moved: false, static: false },
-    { w: 1, h: 2, x: 0, y: 18, i: 'nutrition', moved: false, static: false }
+    { w: 1, h: 2, x: 0, y: 2, minW: 1, minH: 2, i: 'overview', moved: false, static: false },
+    { w: 1, h: 2, x: 0, y: 0, minW: 1, minH: 2, maxH: 2, i: 'calendar', moved: false, static: false },
+    { w: 1, h: 2, x: 0, y: 4, minW: 1, minH: 2, i: 'dailyWater', moved: false, static: false },
+    { w: 1, h: 2, x: 0, y: 6, minW: 1, minH: 2, i: 'activityGoal', moved: false, static: false },
+    { w: 1, h: 2, x: 0, y: 8, minW: 1, minH: 2, i: 'stress', moved: false, static: false },
+    { w: 1, h: 2, x: 0, y: 10, minW: 1, minH: 2, i: 'mood', moved: false, static: false },
+    { w: 1, h: 2, x: 0, y: 12, minW: 1, minH: 2, i: 'sleep', moved: false, static: false },
+    { w: 1, h: 2, x: 0, y: 14, minW: 1, minH: 2, i: 'social', moved: false, static: false },
+    { w: 1, h: 2, x: 0, y: 16, minW: 1, minH: 2, i: 'alcohol', moved: false, static: false },
+    { w: 1, h: 2, x: 0, y: 18, minW: 1, minH: 2, i: 'nutrition', moved: false, static: false }
   ]
 }
 
