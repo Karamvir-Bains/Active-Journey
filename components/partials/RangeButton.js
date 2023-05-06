aaedf2d605af5a10c98cca1c0fcc49de7e654321
Lauren Ashley <la.zaubi@gmail.com>
2023-05-06 15:33:46 -0700

    fix dark mode selected state bug

--- components/partials/RangeButton.js
+++ components/partials/RangeButton.js
@@ -3,13 +3,13 @@ import classNames from "classnames"
 export default function Button(props) {
   let buttonClass = classNames('', {
     'text-blue-700 bg-gray-100 dark:bg-gray-900 dark:text-orange-500' : props.selected,
-    'text-gray-900' : !props.selected
+    'text-gray-900  dark:text-white' : !props.selected
   });
 
   return (
     <span 
       onClick={props.onClick}
-      className={`button px-3 py-1 text-xs font-medium rounded-md border-gray-200 hover:bg-gray-100 dark:focus:text-white dark:focus:ring-blue-500 dark:text-white dark:hover:text-orange-500 dark:hover:border-orange-500 dark:hover:bg-gray-600 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 focus:z-10 hover:text-blue-700 hover:cursor-pointer ${buttonClass}`}
+      className={`button px-3 py-1 text-xs font-medium rounded-md border-gray-200 hover:bg-gray-100 dark:focus:text-white dark:focus:ring-blue-500 dark:hover:text-orange-500 dark:hover:border-orange-500 dark:hover:bg-gray-600 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 focus:z-10 hover:text-blue-700 hover:cursor-pointer ${buttonClass}`}
     >
         {props.range}
     </span>
