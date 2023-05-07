ff4d0dc4bff06cb3f917ca6bca49787513fa9cc7
Lauren Ashley <la.zaubi@gmail.com>
2023-05-06 18:42:11 -0700

    start building first rechart

--- components/widgets/Social.js
+++ components/widgets/Social.js
@@ -1,79 +1,36 @@
-import { useState, useEffect } from "react"
-import { Chart } from "chart.js/auto";
+import { useState, useEffect } from "react";
 import { useTheme } from '../../store/ThemeContext';
 import { palette } from "../../helpers/data";
 import { buildLabels } from "../../helpers/selectors";
 import { useData } from "../../store/DataContext";
 import ZoomButton from "../partials/ZoomButton";
+import { LineChart, Line, XAxis, YAxis, ResponsiveContainer } from 'recharts';
 
 export default function Social(props) {
   const darkMode = useTheme();
   const colours = darkMode === 'light' ? palette.light : palette.dark;  
-  const { 
-    selectedDate,
-    data 
-  } = useData();
+  const { selectedDate, data } = useData();
+
+  const rangeValues = [7, 15, 30];
+  const { range, setRange } = useState(rangeValues[0]);
+  const { widgetData, setWidgetData } = useState();
+
+  const socialData = [
+    {date: 'Page A', uv: 400, pv: 2400, amt: 2400},
+    {date: 'Page A', uv: 500, pv: 2400, amt: 2400},
+    {date: 'Page A', uv: 900, pv: 2400, amt: 2400},
+    {date: 'Page A', uv: 200, pv: 2400, amt: 2400},
+    {date: 'Page A', uv: 400, pv: 2400, amt: 2400}
+  ];
 
   useEffect(() => {
     if (data && data.length && data[7]) {
       const newData = data[7].user_metric_data.slice(-30).map(item => item.metric_value);
-
-      const ctx = document.getElementById("social").getContext('2d');
-
-      const options = {
-        responsive: true,
-        maintainAspectRatio: false,
-        plugins: {
-          legend: { display: false },
-          title: { display: false }
-        },
-        scales: {
-          y: {
-            ticks: { color: colours.label, beginAtZero: true },
-            min: 0
-          },
-          x: {
-            ticks: { color: colours.label, beginAtZero: true }
-          }
-        }
-      };
-
-      const chartData = {
-        labels: buildLabels(selectedDate, 30),
-        datasets: [{
-          data: [...newData],
-          backgroundColor: colours.social,
-          fill: true,
-          tension: 0.378
-        }]
-      }
-
-      const socialChart = new Chart(ctx, {
-        type: 'line',
-        options: options,
-        data: chartData
+      const labels = buildLabels(newData, range);
+      // Build and setWidgetData
+      newData.forEach(element => {
+        
       });
-
-      /** Update chart data anytime we change the selectedDate */
-      socialChart.data.datasets[0].data = [...newData];
-      socialChart.update();
-
-      /** Change chart colours on darkMode change */
-      if (darkMode == 'light') {
-        socialChart.data.datasets[0].backgroundColor = palette.light.social;
-        socialChart.options.scales.x.ticks.color = palette.light.label;
-        socialChart.options.scales.y.ticks.color = palette.light.label;
-        socialChart.update();  
-      } else if (darkMode == 'dark') {
-        socialChart.data.datasets[0].backgroundColor = palette.dark.social;
-        socialChart.options.scales.x.ticks.color = palette.dark.label;
-        socialChart.options.scales.y.ticks.color = palette.dark.label;
-        socialChart.update();  
-      }
-
-      return () => {
-        socialChart.destroy()
-      }
     }
   }, [data, darkMode]);
 
@@ -86,7 +43,13 @@ export default function Social(props) {
           onChange={props.onChange}
         />
         <div className="text-center w-full h-full py-4 mx-auto flex flex-col items-center">
-          <canvas id='social'></canvas>
+        <ResponsiveContainer>
+          <LineChart data={socialData}>
+            <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+            <XAxis dataKey="date" />
+            <YAxis />
+          </LineChart>
+        </ResponsiveContainer>
         </div>
       </div>
     </>
